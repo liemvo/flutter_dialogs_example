89cc79ca9634d8328baae5a1f074d3b5d3ad864d
Liem Vo <liemvt@gmail.com>
2020-08-23 13:11:00 +1200

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dialog',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Dialog'),
+        ),
+        body: Center(child: Text('hello')));
+  }
+}
